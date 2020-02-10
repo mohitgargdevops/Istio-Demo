6f1fc4bb7fc790ec1dd38b818b3fa0b83d4a2c83
jxdemo <mohitgarg.devops@gmail.com>
2020-02-10 14:49:23 +0530

    Application Code Added

---
+++ app1/app1.py
@@ -0,0 +1,14 @@
+from flask import Flask                                                                                             
+import requests
+import os
+import socket                                                                                                       
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_data():
+    url=os.environ["VAR1"]
+    return requests.get(url).content
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
